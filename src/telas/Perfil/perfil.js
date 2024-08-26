bea2de432e24cd4ef5bfcf68d2e8f71139114c2e
TheMatheusVieira <mvieira3845@gmail.com>
2024-08-26 19:46:19 -0300

    att

--- src/telas/Perfil/perfil.js
+++ src/telas/Perfil/perfil.js
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f5f5f5',
-        alignItems: 'center',
+        alignItems: "center",
     },
     video: {
         width: 350,
