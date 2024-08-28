f897b6fc340cb904cbb84eaf68caf3c449533edb
TheMatheusVieira <mvieira3845@gmail.com>
2024-08-27 21:33:43 -0300

    att27.08

--- src/telas/Perfil/perfil.js
+++ src/telas/Perfil/perfil.js
@@ -6,96 +6,105 @@ import {
   TextInput,
   Text,
   Image,
-  Alert,
-  Platform,
 } from "react-native";
-
 import { Camera } from "expo-camera";
-import { Video, ResizeMode } from "expo-av";
+import { Video } from "expo-av";
 
 export default function Perfil() {
-  const [nomeuser, mudaNome] = React.useState("");
-  const [sobrenomeuser, mudaSobrenome] = React.useState("");
-  const [cidadeuser, mudaCidade] = React.useState("");
-  const [profiuser, mudaProfi] = React.useState("");
-
-  const video = React.useRef(null);
-
+  const [nomeuser, mudaNome] = useState("");
+  const [sobrenomeuser, mudaSobrenome] = useState("");
+  const [cidadeuser, mudaCidade] = useState("");
+  const [profiuser, mudaProfi] = useState("");
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [photoUri, setPhotoUri] = useState(null);
+  const [isCameraActive, setIsCameraActive] = useState(false);
   const cameraRef = useRef(null);
 
-  const requestCameraPermission = async () => {
-    if (Platform.OS === "android") {
+  useEffect(() => {
+    const requestCameraPermission = async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasCameraPermission(status === "granted");
-    } else {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasCameraPermission(status === "granted");
-    }
-  };
+    };
+
+    requestCameraPermission();
+  }, []);
 
   const takePhoto = async () => {
     if (cameraRef.current) {
       const photo = await cameraRef.current.takePictureAsync();
       setPhotoUri(photo.uri);
+      setIsCameraActive(false); // Fecha a câmera após tirar a foto
     }
   };
 
-  React.useEffect(() => {
-    requestCameraPermission();
-  }, []);
+  const handleOpenCamera = () => {
+    if (hasCameraPermission) {
+      setIsCameraActive(true);
+    } else {
+      console.log("Permissão da câmera não concedida.");
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.fundouser}>
+      <TouchableOpacity style={styles.cameraContainer} onPress={handleOpenCamera}>
         {photoUri ? (
           <Image source={{ uri: photoUri }} style={styles.profileImage} />
         ) : (
-          <TouchableOpacity style={styles.fundoTouchable} onPress={takePhoto}>
-            <Text style={styles.fundoText}>
-              Clique aqui para tirar uma foto
-            </Text>
-          </TouchableOpacity>
+          <Text style={styles.cameraPlaceholderText}>Clique para abrir a câmera</Text>
         )}
-      </View>
+      </TouchableOpacity>
 
-      <View style={styles.input}>
-        <View>
+      {/* {isCameraActive && ( */}
+         <Camera style={styles.camera} ref={cameraRef}>
+          {/* <View style={styles.cameraButtonContainer}> */}
+            {/* <TouchableOpacity style={styles.cameraButton} onPress={takePhoto}> */}
+              {/* <Text style={styles.cameraButtonText}>Tirar Foto</Text> */}
+            {/* </TouchableOpacity> */}
+          {/* </View> */}
+        </Camera>
+      {/* // )} */}
+
+      <View style={styles.inputContainer}>
+        <View style={styles.inputGroup}>
           <Text>Nome</Text>
           <TextInput
             onChangeText={mudaNome}
             value={nomeuser}
             placeholder="Insira o seu nome"
+            style={styles.input}
           />
         </View>
 
-        <View>
+        <View style={styles.inputGroup}>
           <Text>Sobrenome</Text>
           <TextInput
             onChangeText={mudaSobrenome}
             value={sobrenomeuser}
             placeholder="Insira o seu sobrenome"
+            style={styles.input}
           />
         </View>
       </View>
 
-      <View style={styles.input2}>
-        <View>
+      <View style={styles.inputContainer}>
+        <View style={styles.inputGroup}>
           <Text>Cidade</Text>
           <TextInput
             onChangeText={mudaCidade}
             value={cidadeuser}
             placeholder="Insira a sua cidade"
+            style={styles.input}
           />
         </View>
 
-        <View>
+        <View style={styles.inputGroup}>
           <Text>Profissão</Text>
           <TextInput
             onChangeText={mudaProfi}
             value={profiuser}
             placeholder="Insira a sua profissão"
+            style={styles.input}
           />
         </View>
       </View>
@@ -104,26 +113,15 @@ export default function Perfil() {
         <Text>SALVAR INFORMAÇÕES</Text>
       </TouchableOpacity>
 
-      <View>
+      <View style={styles.videoContainer}>
         <Video
-          ref={video}
           style={styles.video}
           source={require("../../../assets/videolojaapp.mp4")}
           useNativeControls
-          resizeMode={ResizeMode.CONTAIN}
+          resizeMode="contain"
           isLooping
         />
       </View>
-
-      {hasCameraPermission && (
-        <Camera style={styles.camera} ref={cameraRef}>
-          <View style={styles.cameraButtonContainer}>
-            <TouchableOpacity style={styles.cameraButton} onPress={takePhoto}>
-              <Text style={styles.cameraButtonText}>Tirar Foto</Text>
-            </TouchableOpacity>
-          </View>
-        </Camera>
-      )}
     </View>
   );
 }
@@ -133,39 +131,76 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#f5f5f5",
     alignItems: "center",
-    bottom: 20,
-  },
-  video: {
-    width: 350,
-    height: 275,
-    alignSelf: "center",
-  },
-  input: {
-    flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
-    gap: 50,
-    marginBottom: 20,
   },
-  input2: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 50,
+  cameraContainer: {
     marginBottom: 20,
-  },
-  fundouser: {
-    marginBottom: 50,
     width: 150,
     height: 150,
+    borderRadius: 75,
+    overflow: "hidden",
     backgroundColor: "black",
-    borderRadius: 80,
-    marginTop: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  camera: {
+    width: 150,
+    height: 150,
+    position: "absolute",
+    borderRadius: 75,
+  },
+  cameraButtonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cameraButton: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+  },
+  cameraButtonText: {
+    fontSize: 18,
+    color: "black",
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  cameraPlaceholderText: {
+    color: "white",
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    flexWrap: "wrap",
+    gap: 20,
+  },
+  inputGroup: {
+    marginHorizontal: 10,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    marginBottom: 10,
+    paddingHorizontal: 5,
+    width: 150,
   },
   btsalvar: {
     backgroundColor: "grey",
     padding: 10,
     borderRadius: 15,
     marginTop: 20,
+  },
+  videoContainer: {
+    marginTop: 20,
+  },
+  video: {
+    width: 350,
+    height: 275,
   },
 });
